c55affb7c88191c6e127b780625c00a6c22035c3
Marmeus <jesus.rodenash@gmail.com>
2021-11-05 16:29:34 +0100

    Descarga AccessChk.zip y ProcessMonitor.zip

--- kaliBoost.sh
+++ kaliBoost.sh
@@ -187,6 +187,8 @@ wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64 -O ~
 wget https://raw.githubusercontent.com/PowerShellEmpire/PowerTools/master/PowerUp/PowerUp.ps1 -O ~/UTILS/PowerUp.ps1
 wget https://raw.githubusercontent.com/rasta-mouse/Sherlock/master/Sherlock.ps1 -O ~/UTILS/Sherlock.ps1
 wget https://gist.githubusercontent.com/joswr1ght/22f40787de19d80d110b37fb79ac3985/raw/50008b4501ccb7f804a61bc2e1a3d1df1cb403c4/easy-simple-php-webshell.php -O ~/UTILS/sws.php
+wget https://download.sysinternals.com/files/ProcessMonitor.zip -O ~/UTILS/ProcessMonitor.zip
+wget https://download.sysinternals.com/files/AccessChk.zip -O ~/UTILS/AccessChk.zip
 
 echo ######################################################################
 echo                           REINICIANDO
